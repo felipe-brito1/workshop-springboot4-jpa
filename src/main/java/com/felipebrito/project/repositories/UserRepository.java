66b7bb81dfcbd557712d20ddd72c829f4d90c3f4
Felipe Brito <felipebrito0429@gmail.com>
2026-03-15 15:38:56 -0300

    Service layer, component registration

--- src/main/java/com/felipebrito/project/repositories/UserRepository.java
+++ src/main/java/com/felipebrito/project/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.felipebrito.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.felipebrito.project.entities.User;
 
+
+@Repository	
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
