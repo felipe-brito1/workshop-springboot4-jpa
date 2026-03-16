b9ab0c12a296240eed814e55e3e8d0ea563c0125
Felipe Brito <felipebrito0429@gmail.com>
2026-03-16 19:33:30 -0300

    Created entity category

---
+++ src/main/java/com/felipebrito/project/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.felipebrito.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipebrito.project.entities.Category;
+
+
+@Repository	
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ src/main/java/com/felipebrito/project/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.felipebrito.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipebrito.project.entities.Category;
+import com.felipebrito.project.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
